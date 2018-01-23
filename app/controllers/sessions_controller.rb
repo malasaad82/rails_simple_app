aedc2174a8266d90a77f7e639a42951a28f0b182
Mohammed <mohammed@stormfors.se>
2018-01-23 15:27:37 +0100

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # @user For using an instance variable in the create action
   	# Pulls the user out of the database using the submitted email address
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       # log_in => Helper in sessions_helper.rb
-      log_in user
-      redirect_to user      
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user      
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!      
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-	before_save { self.email = email.downcase }
+	attr_accessor :remember_token
+  before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i  
   validates :email, presence: true, length: { maximum: 255 },
@@ -9,11 +10,35 @@ class User < ApplicationRecord
 	validates :password, presence: true, length: { minimum: 6 }
 
  	
-	# Adding a digest method for use in fixtures
- 	# Returns the hash digest of the given string.
+  # Adding a digest method for use in fixtures
+  # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end	
+
+  # Returns a random token. used for remember user login even if close browser
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+   
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
