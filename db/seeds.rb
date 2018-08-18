3bcde675f6527ab91ed03c31d23be98108bef1c5
Mohammed <mohammed@stormfors.se>
2018-08-18 22:07:08 +0200

    Fix seed user generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# User.create!(name:  "Example User",
-#              email: "example@railstutorial.org",
-#              password:              "foobar",
-#              password_confirmation: "foobar",
-#              admin: true,
-#              activated: true,
-#              activated_at: Time.zone.now)
-#
-#
-# 99.times do |n|
-#   name  = Faker::Name.name
-#   email = "example-#{n+1}@railstutorial.org"
-#   password = "password"
-#   User.create!(name:  name,
-#                email: email,
-#                password:              password,
-#                password_confirmation: password,
-#                activated: true,
-#                activated_at: Time.zone.now)
-# end
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
 
 20.times do |n|
   #title  = Faker::Lorem.paragraph[0..24]
